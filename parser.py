770c97dc6c605de68d6b90c2d5d87f461d5d5e07
aigerim055 <omurzakova727@gmail.com>
2022-12-18 00:27:05 +0600

    write to excel file

--- parser.py
+++ parser.py
@@ -2,7 +2,8 @@ import csv
 from bs4 import ResultSet
 import requests
 from bs4 import BeautifulSoup
-from decouple import config
+import xlsxwriter 
+# from decouple import config
 
 
 session = requests.Session()
@@ -23,7 +24,6 @@ data = {
 }
 
 response = requests.post('https://diler.mosplitka.ru/', params=params, data=data)
-# print(response.text)
 
 class Parser:
 
@@ -32,7 +32,6 @@ class Parser:
         url = 'https://diler.mosplitka.ru/catalog/?login=yes'
         response = session.post(url=url, params=params, data=data)
         html = response.text
-        # print(html)
         return html
 
 
@@ -94,9 +93,9 @@ class Parser:
                 desc7 = ''
             
             try:
-                desc8 = card.find_all('div', class_='catalog__pack-weight')
+                weight = card.find_all('div', class_='catalog__pack-weight')
             except AttributeError:
-                desc8 = ''
+                weight = ''
 
             try:
                 price = card.find_all('div', class_='catalog__basic-price')
@@ -113,8 +112,6 @@ class Parser:
             except AttributeError:
                 in_stock_krasnodar = ''
 
-            description = desc1+ desc2 + desc3 + desc4 + desc5 
-            description_packing = desc6 + desc7 + desc8
 
             for a in articul:
                 articul = a.text
@@ -139,34 +136,31 @@ class Parser:
 
             for a in desc6:
                 desc6 = a.text + ' '
-                # print(desc6)
 
             for a in desc7:
                 desc7 = a.text.strip() + ' '
-                # print(desc7)
 
-            for a in desc8:
-                desc8 = a.text.strip() + ' '
-                # print(desc8)
+            for a in weight:
+                weight = a.text.strip()
 
             for a in price:
                 price = a.text.strip()
 
             for a in in_stock_podolsk:
                 in_stock_podolsk = a.text
-                # print(in_stock_podolsk)
 
             for a in in_stock_krasnodar:
                 in_stock_krasnodar = a.text  
 
             description = desc1+ desc2 + desc3 + desc4 + desc5 
-            description_packing = f'{desc6} {desc7} {desc8}'
+            description_packing = f'{desc6} {desc7}'
 
             obj = {
                'articul': articul,
                'title': title,
                'description': description,
                'description_packing': description_packing,
+               'weight': weight,
                'price': price,
                'in_stock_podolsk': in_stock_podolsk,
                'in_stock_krasnodar': in_stock_krasnodar,
@@ -180,38 +174,47 @@ class Parser:
         pages = '?PAGEN_1='
         html = self.get_html()
         pages: ResultSet = html.find('div', class_='navigarion-catalog')#.find('div', class_='navigation-pages').find_all('a')
-        # print(pages)
 
     def get_last_page(html):
         """ Получение количества страниц """
         soup = BeautifulSoup(html, 'lxml')
-        # print(soup)
         total_pages = soup.find('div', class_='navigation-catalog').find('div', class_='navigation-pages').find_all('a')[-2]
-        # a = total_pages.find('div', class_='navigation-catalog')
-        # print(total_pages)
-        # pages = soup.find('div', class_='navigation-catalog').find('div', class_='navigation-pages')
-        # last_page = pages.find_all('span', class_='nav-current-page')[1]
-        # print(last_page)
-        # return int(last_page)
         last_page = total_pages.text
         return int(last_page)
     
     def write_to_csv(data: list):
         """ Запись данных в csv файл """
-        fieldnames = ['articul', 'title', 'description', 'description_packing', 'price', 'in_stock_podolsk', 'in_stock_krasnodar']
+        fieldnames = ['articul', 'title', 'description', 'description_packing', 'weight','price', 'in_stock_podolsk', 'in_stock_krasnodar']
         with open('test.csv', 'w') as file:
             csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
             csv_writer.writeheader()
             csv_writer.writerows(data)
     
-    html = get_html()
-    # print(get_last_page(html))
-    # items = self.soup(html).find('div', class_='navigarion-catalog').find('div', class_='navigation-pages').find_all('a')
+    OUT_XLSX_FILENAME = 'test.xlsx'
+    def write_to_excel(file_name, data):
+        if not len(data):
+            return None
 
-    # html = get_html()
-    # cards = get_card_from_html(html=html)
-    # print(parse_data_from_cards(cards=cards))
-    # print(get_last_page())
+        with xlsxwriter.Workbook(file_name) as workbook:
+            ws = workbook.add_worksheet()
+            bold = workbook.add_format({'bold': True})
+            headers = ['артикул', 'наименование товара', 'описание товара', 'пакинг', 'вес упаковки', 'цена базовая', 'наличие Подольск', 'наличие Краснодар']        
+
+            for col, h in enumerate(headers):
+                ws.write_string(0, col, h, cell_format=bold)
+
+            for row, item in enumerate(data, start=1):
+                ws.write_string(row, 0, item['articul'])
+                ws.write_string(row, 1, item['title'])
+                ws.write_string(row, 2, item['description'])
+                ws.write_string(row, 3, item['description_packing'])
+                ws.write_string(row, 4, item['weight'])
+                ws.write_string(row, 5, item['price'])
+                ws.write_string(row, 6, item['in_stock_podolsk'])
+                ws.write_string(row, 7, item['in_stock_krasnodar'])
+
+
+    html = get_html()
 
     result = []
     for page in range(1, get_last_page(html)):
@@ -219,11 +222,13 @@ class Parser:
         list_of_cards = parse_data_from_cards(cards=cards)
         print(list_of_cards)
         result.extend(list_of_cards)
-        # write_to_csv(result)
+        write_to_excel(OUT_XLSX_FILENAME, result)
         # print(result)
+    #     write_to_csv(result)
+
+
 
 
 if __name__ == '__main__':
     obj = Parser()
     print(obj)
-    # obj
